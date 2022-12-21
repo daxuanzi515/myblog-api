e9847572640454a4c618bad05bc4b8110b60d443
daxuanzi515 <96161750+daxuanzi515@users.noreply.github.com>
2022-12-21 23:20:34 +0800

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ dotenv.config()
 
 //use json
 app.use(express.json());
-app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
+app.use(cors({ origin:["http://localhost:3000","https://myblog-client.vercel.app/"] }));
 app.use("/images",express.static(path.join(__dirname,"/images")))
 
 mongoose.set('strictQuery', true);
